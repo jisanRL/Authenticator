03a642d709b75d208ce1bcc680bea17b9336102f
jisanRL <>
2021-12-20 15:32:16 -0500

    admin table is ready, connect front-end and backend now

--- authenticatorFrontend/src/app/admin/admin.component.ts
+++ authenticatorFrontend/src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from '../model/User';
 
 @Component({
   selector: 'app-admin',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+
+  users: User[] = [];
+  editUser: User = new User();
+  deleteUser: User = new User();
 
   constructor() { }
 
@@ -29,4 +35,40 @@ export class AdminComponent implements OnInit {
 
   }
 
+  // add the employee to the server
+  onAddUser(addForm: NgForm):void {
+    document.getElementById('add-user-form')?.click();
+
+  }
+
+  //updates/edits user
+  onUpdateUser(user: User):void{
+
+  }
+
+  //deletes the user 
+  onDeleteUser(usedID: number) {
+
+  }
+
+  // opens the modal
+  onOpenModal(user: User, mode:String):void {
+    const container = document.getElementById('dt-vertical-scroll');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+
+    if(mode === 'add'){
+      button.setAttribute('data-target', '#addUserModal')
+    }
+    if(mode === 'edit'){
+      button.setAttribute('data-target', '#updateUserModal')
+    }
+    if(mode === 'delete') {
+      button.setAttribute('data-target', '#deleteUserModal')
+    }
+    container?.appendChild(button);
+    button.click();
+  }
 }
---
+++ authenticatorFrontend/src/app/model/User.ts
@@ -0,0 +1,10 @@
+export class User{
+    // change this later and map it with the databases entity
+    id : number = 0;
+    name: String = "";
+    email : String = "";
+    jobTitle : String = "";
+    phone  : String = "";
+    imageURL : String = "";
+    employeeCode  : String = "";
+}
